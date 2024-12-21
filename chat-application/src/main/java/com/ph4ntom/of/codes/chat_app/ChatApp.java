bf9507ff0ff6ad4f2029fa47e87561d71b2cdd79
Subhatav Dhali <subhatav@gmail.com>
2024-12-21 16:32:30 +0530

    Establish a Spring Boot project with Maven project manager

---
+++ chat-application/src/main/java/com/ph4ntom/of/codes/chat_app/ChatApp.java
@@ -0,0 +1,12 @@
+package com.ph4ntom.of.codes.chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApp {
+
+  public static void main(final String[] arguments) {
+    SpringApplication.run(ChatApp.class, arguments);
+  }
+}
---
+++ chat-application/src/test/java/com/ph4ntom/of/codes/chat_app/ChatAppTests.java
@@ -0,0 +1,13 @@
+package com.ph4ntom.of.codes.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppTests {
+
+  @Test
+  void contextLoads() {
+    //TODO: Tests will be implemented later
+  }
+}
